bc254efc9c512712c4ae66ff394eef1fa54302c5
EmirOfSwing <emir.arh@gmail.com>
2021-03-11 03:18:58 +0100

    Added Exercise number 2

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code bellow this line
+    // Comment number one
+    /* Comment number two*/
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
