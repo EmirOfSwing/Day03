ce47c26e4b1012e9e30f4422c5a05cdb3e5e34fe
EmirOfSwing <emir.arh@gmail.com>
2021-03-11 03:46:45 +0100

    Added Exercise 4

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code bellow this line
+var x;
+var Y;
+
+x = 10;
+Y = x;
+    //Only change code above this line
+return [x, y];
+
+}
+
+console.log(main());
+
+module.exports = main;
