a5aa68db7a8d0fa9fb0eeb0774114b0f084ae7e5
EmirOfSwing <emir.arh@gmail.com>
2021-03-11 05:15:20 +0100

    Added Exercise 09

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+
+    var difference
+    difference = numOne - numTwo;
+
+    return difference;
+}
+
+console.log(main(numOne = 15, numTwo = 8)); 
+console.log(main(numOne = 25, numTwo = 8)); 
+console.log(main(numOne = 425, numTwo = 120)); 
+
+module.exports = main;
